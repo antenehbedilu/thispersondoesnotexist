2262a77199a6ffa652e23cae153f9b6ac999bac8
Anteneh Bedilu <anteneh@antenehbedilu.com>
2023-11-06 22:43:15 +0000

    feat(backend/main.py): add endpoint to fetch and stream human faces that do not actually exist

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,13 @@ app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi', redoc_url=None, 
 def health():
     #returns a JSON response with a status of "OK"
     return {'status': 'OK'}
+
+#endpoint for fetching and streaming an image of a person that doesn't exist; from ThisPersonDoesNotExist API
+@app.get('/api/person', tags=['Person'])
+def image():
+    #URL of the image to fetch
+    image_url = 'https://thispersondoesnotexist.com'
+    #fetch the image content from the URL
+    response = get(image_url, stream=True)
+    #return the image as a streaming response
+    return StreamingResponse(response.iter_content(chunk_size=1024), media_type='image/jpeg')
